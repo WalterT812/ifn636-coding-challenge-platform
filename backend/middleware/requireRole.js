1d0194d83fd0989f0079be1368ce1b9e2a9ab267
Walter Tang <waltert812@outlook.com>
2026-09-04 07:11:07 +1000

    CCP-33 add backend role middleware

---
+++ backend/middleware/requireRole.js
@@ -0,0 +1,15 @@
+function requireRole(...roles) {
+    return function (req, res, next) {
+        if (!req.user) {
+            return res.status(401).json({ message: 'Please login first' });
+        }
+
+        if (!roles.includes(req.user.role)) {
+            return res.status(403).json({ message: 'Forbidden' });
+        }
+
+        next();
+    };
+}
+
+module.exports = requireRole;
